e844d3a2ab10c9b324ae81e59e122877d3b2cdd4
YOUR_NAME <evgeniachu130801@gmail.com>
2023-04-30 23:08:02 +0500

    Added melee weapon

---
+++ ParasiteTheGame/Assets/Scripts/MeleeWeapon.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MeleeWeapon : AWeapon
+{
+    private LayerMask damageTakerLayers;
+    private bool didDamage = false;
+
+    protected override void Fire(InputInfo inpInf)
+    {
+        if (user.GetDamageSource() is DamageSource.Player)
+            damageTakerLayers = LayerMask.GetMask("Controllables");
+        else
+            damageTakerLayers = LayerMask.GetMask("Player");
+        var enemies = Physics2D.OverlapCircleAll(transform.position, 0.6f, damageTakerLayers);
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            didDamage = enemies[i].GetComponent<AEnemy>().TryTakeDamage(new DamageInfo(DamageType.Melee, damageSource, 4));
+        }
+    }
+}
