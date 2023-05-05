d837fdd1d64db1ddac60ac513722a0eaa9646fdf
Alexey_Dzhevello <fishwere@mail.ru>
2023-05-05 10:43:04 +0500

    add lackey

---
+++ ParasiteTheGame/Assets/Scripts/GameObjects/Enemies/LackeySmall/LackeySmall.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LackeySmall : AEnemy
+{
+    Animator animator;
+    protected override void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        radius = 2;
+        animator = GetComponent<Animator>();
+    }
+
+    public override void ControlledUpdate(InputInfo inpInf)
+    {
+        myRigidbody.velocity = inpInf.Axis * velocity;
+        //
+        animator.SetFloat("moveX", inpInf.Axis.x);
+        animator.SetFloat("moveY", inpInf.Axis.y);
+        if (inpInf.Axis.x != 0 || inpInf.Axis.y != 0)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+        //
+        if (item != null)
+        {
+            item.HandleUpdate(inpInf);
+            if (inpInf.ThrowItemPressed)
+            {
+                item.Throw(inpInf);
+                item = null;
+            }
+        }
+    }
+
+    public override void OnCapture(PlayerController player)
+    {
+        IsCaptured = true;
+        //
+        player.GetComponent<SpriteRenderer>().enabled = false;
+        animator.SetBool("isUncontious", false);
+        GetComponent<LackeySmallAI>().enabled = false;
+        //
+    }
+
+    public override void OnRelease(PlayerController player)
+    {
+        myRigidbody.velocity = new Vector2(0, 0);
+        IsCaptured = false;
+        //
+        player.GetComponent<SpriteRenderer>().enabled = true;
+        if (health <= 0)
+        {
+            animator.SetBool("isUncontious", true);
+        }
+        else
+        {
+            GetComponent<LackeySmallAI>().enabled = true;
+        }
+        animator.SetBool("isMoving", false);
+        //
+    }
+
+    public override bool TryTakeDamage(DamageInfo dmgInf)
+    {
+        if ((IsCaptured && dmgInf.Source == DamageSource.Enemy)
+            || (!IsCaptured && dmgInf.Source == DamageSource.Player)
+            || dmgInf.Source == DamageSource.Environment)
+        {
+            health -= dmgInf.Amount;
+            Debug.Log($"Enemy hurt : health = {health}");
+            //
+            if (health <= 0)
+            {
+                animator.SetBool("isUncontious", true);
+                GetComponent<LackeySmallAI>().enabled = false;
+            }
+            else
+            {
+                animator.SetBool("isUncontious", false);
+            }
+            //
+            return true;
+        }
+        return false;
+    }
+
+    protected override void PickUp()
+    {
+        var t = Physics2D.OverlapCircle(transform.position, itemPickingRadius, Constants.PickableItems);
+        if (t)
+        {
+            item = t.gameObject.GetComponent<IUsable>();
+            item?.OnPickUp(this);
+        }
+        else
+        {
+            //
+            //Debug.Log("nothing to pick up");
+            //
+        }
+    }
+
+    public bool HaveItem => item is not null;
+}
---
+++ ParasiteTheGame/Assets/Scripts/GameObjects/Enemies/LackeySmall/LackeySmallAI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LackeySmallAI : MonoBehaviour
+{
+    LackeySmall lackey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lackey = GetComponent<LackeySmall>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = new Vector2(Random.value*2 - 1, Random.value*2 - 1).normalized;
+        var inpInf = new InputInfo(new Vector2(Random.Range(-1, 2), Random.Range(-1, 2)), direction, false, !lackey.HaveItem, true, false);
+        lackey.ControlledUpdate(inpInf);
+    }
+}
