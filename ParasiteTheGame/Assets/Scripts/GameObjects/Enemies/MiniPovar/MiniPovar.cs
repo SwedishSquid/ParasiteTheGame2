d71740ebb8838fdfaf254b0145c8fa4f028e7fd8
YOUR_NAME <evgeniachu130801@gmail.com>
2023-05-22 20:38:01 +0500

    Add some fix

---
+++ ParasiteTheGame/Assets/Scripts/GameObjects/Enemies/MiniPovar/MiniPovar.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniPovar : AEnemyPlus
+{
+    protected override void Start()
+    {
+        base.Start();
+        radius = 2;
+    }
+
+    public override void ControlledUpdate(InputInfo inpInf)
+    {
+        base.ControlledUpdate(inpInf);
+        //
+        animator.SetFloat("moveX", inpInf.Axis.x);
+        animator.SetFloat("moveY", inpInf.Axis.y);
+        if (freezeTime <= 0 && (inpInf.Axis.x != 0 || inpInf.Axis.y != 0))
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+        //
+    }
+
+    public override void OnCapture(PlayerController player)
+    {
+        base.OnCapture(player);
+        //
+        animator.SetBool("isUncontious", false);
+        GetComponent<MiniPovarAI>().enabled = false;
+        //
+    }
+
+    public override void OnRelease(PlayerController player)
+    {
+        base.OnRelease(player);
+        //
+        if (health <= 0)
+        {
+            animator.SetBool("isUncontious", true);
+        }
+        else
+        {
+            GetComponent<MiniPovarAI>().enabled = true;
+        }
+        animator.SetBool("isMoving", false);
+        //
+    }
+
+    public override bool TryTakeDamage(DamageInfo dmgInf)
+    {
+        var result = base.TryTakeDamage(dmgInf);
+        if (result)
+        {
+            if (health <= 0)
+            {
+                animator.SetBool("isUncontious", true);
+                GetComponent<MiniPovarAI>().enabled = false;
+                if (item != null)
+                {
+                    DropDown();
+                }
+            }
+            else
+            {
+                healthBar.SetValue(health);
+                animator.SetBool("isUncontious", false);
+                animator.SetBool("isMoving", false);
+            }
+        }
+        return result;
+    }
+}
