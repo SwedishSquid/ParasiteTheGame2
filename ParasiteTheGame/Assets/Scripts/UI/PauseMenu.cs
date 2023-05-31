9575e0175096b6cff974cf2d5d6805b9c95e5a35
Evgeny Lapshin <zhenia.lapshin201@yandex.ru>
2023-05-31 23:52:15 +0500

    add pauseMenu

---
+++ ParasiteTheGame/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu: MonoBehaviour
+{
+    [SerializeField] private Canvas pauseCanvas;
+    [SerializeField] private Button saveGame;
+    [SerializeField] private Button mainMenu;
+
+    public void PressedPause()
+    {
+        if (!PauseController.gameIsPaused)
+        {
+            pauseCanvas.gameObject.SetActive(false);
+            return;
+        }
+        pauseCanvas.gameObject.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(saveGame.gameObject);
+    }
+
+    public void PressedSaveGame()
+    {
+        DataPersistenceManager.Instance.SaveGame();
+    }
+
+    public void PressedMainMenu()
+    {
+        DisableAllButtons();
+        pauseCanvas.gameObject.SetActive(false);
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+
+    private void DisableAllButtons()
+    {
+        saveGame.interactable = false;
+        mainMenu.interactable = false;
+    }
+    
+}
