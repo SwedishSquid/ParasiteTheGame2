7071e3baba0957b24e555d93327413a2907cf560
ITSigma <ascoolasanton@gmail.com>
2023-06-01 12:57:43 +0500

    sprites and empty obj Kitchener Super

---
+++ ParasiteTheGame/Assets/Scripts/GameObjects/Enemies/Kitchener/Kitchener.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kitchener : AEnemyPlus
+{
+    float lastX;
+    float lastY;
+    Type AIType = typeof(GardenerAI); //ITSigma TODO : Change to KitchenerAI
+    private float maxAttackCooldown = 4f; 
+    private float attackCooldown;
+    [SerializeField] KitchenerSuperAttack superAttack;
+
+    protected override void Start()
+    {
+        base.Start();
+        radius = 2;
+    }
+
+    public override void ControlledUpdate(InputInfo inpInf)
+    {
+        base.ControlledUpdate(inpInf);
+        //
+        attackCooldown -= Time.deltaTime;
+        //
+        if (!PauseController.gameIsPaused)
+        {
+            animator.SetFloat("moveX", inpInf.Axis.x);
+            animator.SetFloat("moveY", inpInf.Axis.y);
+            lastX = inpInf.Axis.x;
+            lastY = inpInf.Axis.y;
+        }
+        else
+        {
+            animator.SetFloat("moveX", lastX);
+            animator.SetFloat("moveY", lastY);
+            return;
+        }
+        //
+        if (freezeTime <= 0 && (inpInf.Axis.x != 0 || inpInf.Axis.y != 0))
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+        //
+        if (damageSource == DamageSource.Player 
+            && Input.GetButtonDown("SuperAttack") 
+            && attackCooldown <= 0)
+        {
+            attackCooldown = maxAttackCooldown;
+            superAttack.Attack(damageSource, this);
+        }
+    }
+
+    public override void OnCapture(PlayerController player)
+    {
+        base.OnCapture(player);
+        //
+        animator.SetBool("isUncontious", false);
+        GetComponent<AIType>().enabled = false;
+        //
+    }
+
+    public override void OnRelease(PlayerController player)
+    {
+        base.OnRelease(player);
+        //
+        if (health <= 0)
+        {
+            animator.SetBool("isUncontious", true);
+        }
+        else
+        {
+            GetComponent<AIType>().enabled = true;
+        }
+        animator.SetBool("isMoving", false);
+        //
+    }
+
+    public override bool TryTakeDamage(DamageInfo dmgInf)
+    {
+        var result = base.TryTakeDamage(dmgInf);
+        if (result)
+        {
+            if (health <= 0)
+            {
+                animator.SetBool("isUncontious", true);
+                GetComponent<AIType>().enabled = false;
+                if (item != null)
+                {
+                    DropDown();
+                }
+            }
+            else
+            {
+                healthBar.SetValue(health);
+                animator.SetBool("isUncontious", false);
+                animator.SetBool("isMoving", false);
+            }
+        }
+        return result;
+    }
+}
